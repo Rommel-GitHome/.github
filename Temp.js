25a2ce7372314d2dfda46936a96581a9138d240b
Rommel-GitHome <jantri@gmail.com>
2022-11-08 23:54:04 +0100

    Add files via upload

---
+++ Temp.js
@@ -0,0 +1,18 @@
+import { Selector } from "testcafe";
+
+fixture`Navigate to an App`
+    .disablePageReloads
+    .page(`https://planet9dev.neptune-software.com`)
+
+test(`TC-03-1001-022: Sample Test`, async t => {
+    try {
+        await t
+            .typeText(Selector('input#inLoginName-inner'), 'testcafe', { paste: true })
+            .typeText(Selector('input#inLoginPassword-inner'), 'testcafePassword', { paste: true })
+            .click(Selector('button#butLogin'));
+
+        t.ctx.passed = true;
+    } catch (err) {
+        throw err;
+    }
+});
